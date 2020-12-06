d521ceb657f84d2d465f679a3d81baa5031ee91c
higuchi-yuya-ab <higuchi_yuya@applibot.co.jp>
2020-12-06 16:35:35 +0900

    fix bubble sort

--- src/sort_lib.rs
+++ src/sort_lib.rs
@@ -1,9 +1,9 @@
 pub mod sort {
     pub fn bubble_sort(array: &mut [i32]) {
         for i in 0..array.len() {
-            for j in i + 1..array.len() {
-                if array[i] > array[j] {
-                    array.swap(i, j);
+            for j in (i + 1..array.len()).rev() {
+                if array[j - 1] > array[j] {
+                    array.swap(j, j - 1);
                 }
             }
         }
