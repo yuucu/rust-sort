8deeec9b91a6b944ba10de53aadd3dd6c9b5f318
higuchi-yuya-ab <higuchi_yuya@applibot.co.jp>
2020-12-07 10:27:25 +0900

    add quick sort

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,13 @@ mod sort_lib;
 pub use crate::sort_lib::sort;
 
 fn main() {
-    let mut test1: Vec<i32> = vec![5, 4, 9, 2, 1];
-    sort::bubble_sort(&mut test1);
-    println!("{}", test1.iter().map(|i| i.to_string()).collect::<Vec<_>>().join(","));
+
+    let mut bubble_test: Vec<i32> = vec![5, 10, 9, 2, 1, 4, -5];
+    sort::bubble_sort(&mut bubble_test);
+    println!("{}", bubble_test.iter().map(|i| i.to_string()).collect::<Vec<_>>().join(","));
+
+    let mut quick_test: Vec<i32> = vec![7, 8, 9, 0, 1, -1, 10];
+    let quick_test_num = quick_test.len() - 1;
+    sort::quick_sort(&mut quick_test, 0, quick_test_num);
+    println!("{}", quick_test.iter().map(|i| i.to_string()).collect::<Vec<_>>().join(","));
 }
--- src/sort_lib.rs
+++ src/sort_lib.rs
@@ -1,4 +1,5 @@
 pub mod sort {
+
     pub fn bubble_sort(array: &mut [i32]) {
         for i in 0..array.len() {
             for j in (i + 1..array.len()).rev() {
@@ -8,4 +9,29 @@ pub mod sort {
             }
         }
     }
+
+
+    pub fn quick_sort(array: &mut [i32], start: usize, end: usize) {
+        if start < end {
+            let split = partition(array, start, end);
+            if split > 0 {
+                quick_sort(array, start, split - 1);
+            }
+            quick_sort(array, split + 1, end);
+        }
+    }
+
+    fn partition(array: &mut [i32], p: usize, r: usize) -> usize {
+        let pivot = array[r];
+        let mut i = p;
+        for j in p..r {
+            if array[j] <= pivot {
+                array.swap(i, j);
+                i = i + 1;
+            }
+        }
+        array.swap(i, r);
+        return i
+    }
+
 }
